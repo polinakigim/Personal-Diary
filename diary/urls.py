11e871ab48131319475bc3e55b5aa35ffd42d097
polinakigim <polinakigim@gmail.com>
2025-06-17 04:46:02 +0300

    Второй коммит. Сделаны html файлы, с подключенным css и js: главная страница(home.html), особенности веб-приложения(features.html) и страница для контактов(contacts.html). Выделен отдельный базовый шаблон с повторяющимися элементами(base.html)

--- diary/urls.py
+++ diary/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from diary.apps import DiaryConfig
+from diary.views import home, dashboard, features, contacts
 
 app_name = DiaryConfig.name
 
 urlpatterns = [
-    path('', )
+    path('home/', home, name='home'),
+    path('dashboard/', dashboard, name='dashboard'),
+    path('contacts/', contacts, name='contacts'),
+    path('features/', features, name='features'),
 ]
