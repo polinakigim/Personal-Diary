55453cddb8650086962ae4f5229a4a77138a6ba6
polinakigim <polinakigim@gmail.com>
2025-06-18 21:02:31 +0300

    Создана модель в приложении diary - Entry(запись), в которой есть контент, заголовок и владелец. Реализованы CRUD и соответствующие html шаблоны, доработана главная страница, есть функционал, что только авторизованные пользователи могут владеть записями в дневнике. Entry добавлен в админку Django. Реализованы соответствующие

--- diary/admin.py
+++ diary/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from diary.models import Entry
+
+
+@admin.register(Entry)
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "content", "owner")
+    list_filter = ("title",)
+    search_fields = (
+        "owner",
+        "title",
+    )
--- diary/models.py
+++ diary/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Entry(models.Model):
+    title = models.CharField(
+        max_length=100, blank=True, null=True, verbose_name="Заголовок"
+    )
+    content = models.TextField(verbose_name="Содержимое")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = (
+        models.DateTimeField(auto_now=True, verbose_name="Дата последнего изменения"),
+    )
+    owner = models.ForeignKey(
+        User, verbose_name="Владелец", blank=True, null=True, on_delete=models.SET_NULL
+    )
+
+    class Meta:
+        verbose_name = "Запись"
+        verbose_name_plural = "Записи"
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.content[:50]} — {self.created_at}"
